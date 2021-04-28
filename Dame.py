aabed85507667c95dc50aa7f77e38684ae895a75
mars-31 <mwolff@europe.com>
2021-04-28 12:58:55 +0200

    move_player rework

--- Dame.py
+++ Dame.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 
 class Dame:
@@ -7,15 +8,15 @@ class Dame:
 
     displayname = "Dame"
 
-    startKI = [[1,1],[2,2],[1,3],[2,4],[1,5],[2,6]]
-    startPlayer = [[5,1],[6,2],[5,3],[6,4],[5,5],[6,6]]
+    startKI = [[1, 1], [2, 2], [1, 3], [2, 4], [1, 5], [2, 6]]
+    startPlayer = [[5, 1], [6, 2], [5, 3], [6, 4], [5, 5], [6, 6]]
 
     # Feld initialisieren, aktuellen Spieler auf 'x' setzen
     def __init__(self):
-        self.board = [[' ', ' ', ' ', ' ', ' ', ' '],
-                      ['x', ' ', ' ', ' ', 'o', ' '],
+        self.board = [[' ', 'o', ' ', 'o', ' ', 'o'],
+                      ['o', ' ', 'o', ' ', 'o', ' '],
                       [' ', ' ', ' ', ' ', ' ', ' '],
-                      [' ', ' ', 'o', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' '],
                       [' ', 'x', ' ', 'x', ' ', 'x'],
                       ['x', ' ', 'x', ' ', 'x', ' ']]
         self.length = 6
@@ -31,10 +32,10 @@ class Dame:
         return self.startPlayer
 
     def positionKI(self):
-        return [[1,1],[2,2],[1,3],[2,4],[1,5],[2,6]]
+        return [[1, 1], [2, 2], [1, 3], [2, 4], [1, 5], [2, 6]]
 
     def positionPlayer(self):
-        return [[5,1],[6,2],[5,3],[6,4],[5,5],[6,6]]
+        return [[5, 1], [6, 2], [5, 3], [6, 4], [5, 5], [6, 6]]
 
     # Konsolen-Ausgabe des Feldes
     def print_board(self):
@@ -50,12 +51,44 @@ class Dame:
             print("------------------------------------------")
         print()
 
+    def move_player(self):
+        print('Spieler ' + self.current_player + ' am Zug')
+
+        # alle möglichen Züge ziehen
+        possible_moves = self.get_movable_figures()
+        print("Positionen beweglicher Figuren: ")
+        print(possible_moves)
+
+        start = int(input())
+        valid_move = False
+        # solange ausführen bis User einen validen Zug gewählt hat
+        while not valid_move:
+            possible_moves_clicked = self.get_possible_moves_clicked(start // 10, start % 10)
+            print("Wohin Figur ziehen?")
+            print(possible_moves_clicked)
+            end = int(input())
+            # Eingabe für Koordinaten formatieren, Zehner und EInser separieren
+            move = [end // 10, end % 10]
+            # vergleichen ob Usereingabe ein gültiger Zug ist
+            if move in possible_moves_clicked:
+                self.make_move([[start // 10, start % 10], move])
+                valid_move = True
+
+    def move_computer_random(self):
+        print('Bot mit ' + self.current_player + ' am Zug')
+
+        possible_moves = self.get_all_possible_moves()
+        print("Liste möglicher Züge: ")
+        print(possible_moves)
+
+        self.make_move(random.choice(possible_moves))
+
     # Konditionen für einen Sieg bestimmen
     # 1. Keine Züge mehr möglich
     # 2. Oberen/Unteren Rand mit einer Figur erreicht
     # 3. Kein Stein mehr vorhanden
     def check_winning_conditions(self):
-        if len(self.get_possible_moves()) == 0:
+        if len(self.get_all_possible_moves()) == 0:
             if self.current_player == 'x':
                 return 'o'
             else:
@@ -88,7 +121,59 @@ class Dame:
         else:
             self.current_player = 'x'
 
-    def get_possible_moves(self):
+    def get_movable_figures(self):
+        possible_moves = self.get_all_possible_moves()
+        movable_figures = [i[0] for i in possible_moves]
+        movable_figures_no_dupe = []
+        for i in movable_figures:
+            i = tuple(i)
+            movable_figures_no_dupe.append(i)
+        return list(dict.fromkeys(movable_figures_no_dupe))
+
+    def get_possible_moves_clicked(self, row, col):
+        # Wert der Figuren des anderen Spielers setzen
+        if self.current_player == 'o':
+            enemy_player = 'x'
+        else:
+            enemy_player = 'o'
+
+        # Koordinaten aller möglichen Nachbarn der aktuellen Position
+        if self.current_player == 'x':
+            neighbors = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+        else:
+            neighbors = [[1, -1], [1, 1], [-1, -1], [-1, 1]]
+
+        # Liste für normale Züge
+        possible_moves = []
+        # zweite Liste, falls ein Gegner in diesem Zug geschlagen werden kann
+        possible_moves_with_enemy = []
+        # für alle Elemente innerhalb des Feldes
+        if self.board[row][col].lower() == self.current_player:
+            for index in range(4):
+                if self.board[row][col].islower() and index > 1:
+                    continue
+
+                new_row = row + neighbors[index][0]
+                new_col = col + neighbors[index][1]
+                if 0 <= new_row < len(self.board) and 0 <= new_col < len(self.board[row]):
+                    # alle möglichen Züge OHNE gegnerischen Kontakt der Liste hinzufügen
+                    if self.board[new_row][new_col] == ' ':
+                        possible_moves.append([new_row, new_col])
+                    # wenn ein Gegner geschlagen werden kann wird eine seperate Liste erstellt
+                    elif self.board[new_row][new_col].lower() == enemy_player:
+                        new_row = new_row + neighbors[index][0]
+                        new_col = new_col + neighbors[index][1]
+                        if 0 <= new_row < len(self.board) and 0 <= new_col < len(self.board[row]) and \
+                                self.board[new_row][new_col] == ' ':
+                            possible_moves_with_enemy.append([new_row, new_col])
+
+        # wenn ein Gegner geschlagen werden kann, dann muss dies priorisiert werden
+        if len(possible_moves_with_enemy) > 0:
+            return possible_moves_with_enemy
+        else:
+            return possible_moves
+
+    def get_all_possible_moves(self):
         # Wert der Figuren des anderen Spielers setzen
         if self.current_player == 'o':
             enemy_player = 'x'
